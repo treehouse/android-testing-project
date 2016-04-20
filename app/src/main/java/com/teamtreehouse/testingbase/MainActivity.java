9a102f8fe10e19988d5847564974e695c85b277b
bfdeitch <Ben@Bens-MBP.home>
2016-04-20 09:56:20 -0700

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/testingbase/MainActivity.java
@@ -0,0 +1,82 @@
+package com.teamtreehouse.testingbase;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout bg;
+    EditText editText;
+    TextView textView;
+    Spinner colorSpinner;
+    Button launchActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+        colorSpinner = (Spinner) findViewById(R.id.colorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.colors_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(adapter);
+
+        launchActivityButton = (Button) findViewById(R.id.launchActivityButton);
+        bg = (LinearLayout) findViewById(R.id.bg);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView tv, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    textView.setText(tv.getText().toString());
+                }
+                return false;
+            }
+        });
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        bg.setBackgroundColor(Color.WHITE);
+                        break;
+                    case 1:
+                        bg.setBackgroundColor(Color.MAGENTA);
+                        break;
+                    case 2:
+                        bg.setBackgroundColor(Color.GREEN);
+                        break;
+                    case 3:
+                        bg.setBackgroundColor(Color.CYAN);
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        launchActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
